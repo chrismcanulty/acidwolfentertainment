a9d9cde1c23b51d8d9f55ffe70dc890dca6eb3fb
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2024-02-19 21:20:59 +0900

    build out navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ThemeProvider } from '@mui/material/syles';
+import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
 
