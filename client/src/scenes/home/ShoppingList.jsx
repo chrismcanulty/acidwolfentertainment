5ef3b8f2d34ac7bf2f47ce30c0d72e92422ed328
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2024-03-04 23:44:36 +0900

    create shopping list component to render under main carousel and test that data is being pulled into the component by Strapi by checking console.log for items

---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, Typography, Tab, Tabs, useMediaQuery } from '@mui/material';
+import Item from '../../components/Item';
+import { setItems } from '../../state';
+
+const ShoppingList = () => {
+  const dispatch = useDispatch();
+  // value for filter function
+  const [value, setValue] = useState('all');
+  const items = useSelector((state) => state.cart.items);
+  const isNonMobile = useMediaQuery('(min-width:600px)');
+  console.log('🚀 ~ ShoppingList ~ items:', items);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  // grab item list and images we have created
+  async function getItems() {
+    const items = await fetch(
+      'http://localhost:1337/api/items?populate=image',
+      { method: 'GET' }
+    );
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div>shopping list</div>;
+};
+
+export default ShoppingList;
